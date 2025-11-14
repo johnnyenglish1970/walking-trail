34209bca85d1e223ca21ad436d4f001ee059f611
johnnyenglish1970 <jroyle@btinternet.com>
2025-11-14 10:54:04 +0000

    Update spots.js

--- spots.js
+++ spots.js
@@ -60,6 +60,10 @@ const spots = [
       <p>The Adastral Acoustic Block includes anechoic chambers and a reverberation room and is an important facility for the site to allow audio experimentation and measurement – a key part of telecommunications.</p>
 <br><p>Listen to Chris Adams describe how an anechoic chamber and reverberation room works and how they are used. </p>
        <br> <audio controls src="audio/accoustic1.mp3"></audio>
+        <br><br>
+    <img src="images/2-anechoic -chamber.jpg" class="info-img">
+    <br><br><img src="images/2-anechoic -chamber.jpg" class="info-img">
+       
     `,
   },
   {
@@ -90,10 +94,13 @@ const spots = [
     <p>Building B1, visible to the west, is one of the former RAF Martlesham Heath buildings from WW2 and is now part of Innovation Martlesham. The building is rumoured to be haunted.</p>
 
 <br><p><strong>Listen to Mike Warden talk about sightings of the ghost of B1:</strong><br>
-<audio controls src="audio/b1ghost.mp3"></audio></p>
+<br><audio controls src="audio/b1ghost.mp3"></audio></p>
 
 <br><p><strong>Listen to Bernie Fenn talk about trying to spot the ghost:</strong><br>
-<audio controls src="audio/b1haunted.mp3"></audio></p>
+<br><audio controls src="audio/b1haunted.mp3"></audio></p>
+<br><br><img src="images/1-b1-old.jpg" class="info-img">
+<br><br><img src="images/1-b1-today.jpg" class="info-img">
+
 `,
   },
   {
@@ -104,13 +111,19 @@ const spots = [
      info: ` 
        <p>Beneath these slabs lies one of the first microwave waveguides installed by the Post Office Research Centre. One of the first research divisions to move from Dollis Hill was the waveguide transmission division.</p>
 
-<p>Microwave signals were transmitted through precision-engineered piping, with a wire helix wrapped around the outside to contain the energy. These buried waveguides were seen as more reliable and faster than transmitting through the air.</p>
+<br><p>Microwave signals were transmitted through precision-engineered piping, with a wire helix wrapped around the outside to contain the energy. These buried waveguides were seen as more reliable and faster than transmitting through the air.</p>
 
-<p>A later system connected Portman Road exchange in Ipswich with Wickham Market, carrying live calls through the 1970s and 80s. But by the mid-1970s, fibre-optic cable — another form of waveguide — proved superior: higher capacity, smaller, cheaper, and easier to install.</p>
+<br><p>A later system connected Portman Road exchange in Ipswich with Wickham Market, carrying live calls through the 1970s and 80s. But by the mid-1970s, fibre-optic cable — another form of waveguide — proved superior: higher capacity, smaller, cheaper, and easier to install.</p>
 
-<p>BT became a world leader in fibre-optic technology during the late 1970s and 1980s.</p>
+<br><p>BT became a world leader in fibre-optic technology during the late 1970s and 1980s.</p>
 
-<p>Scroll the slider images below to see installation of the waveguide project.</p>
+<br><p>Scroll the slider images below to see installation of the waveguide project.</p>
+<br><br><img src="images/5-waveguide.jpg" class="info-img">
+<br><br><img src="images/5-waveguide2.jpg" class="info-img">
+<br><br><img src="images/5-waveguide3.jpg" class="info-img">
+<br><br><img src="images/5-waveguide4.jpg" class="info-img">
+<br><br><img src="images/5-waveguide5.jpg" class="info-img">
+<br><br><img src="images/5-waveguide6.jpg" class="info-img">
 `,
   },
   {
@@ -122,9 +135,9 @@ const spots = [
      <p>This facility originally housed subsea cable testing, which is why the 12.5-tonne gantry crane still remains. Today the building supports robotics and drone R&D and contains indoor testbeds that simulate real-world civil engineering environments such as ducts, poles, and underground chambers.</p>
 
 <br><p><strong>Listen to Jon Wakeling talk about current research:</strong><br>
-<audio controls src="audio/robotics.mp3"></audio></p>
+<br><audio controls src="audio/robotics.mp3"></audio></p>
 
-<p><strong>Listen to Bruce Boxall discuss his silicon work in the submarine network:</strong><br>
+<br><p><strong>Listen to Bruce Boxall discuss his silicon work in the submarine network:</strong><br>
 <audio controls src="audio/silicon.mp3"></audio></p>
 `,
   },
@@ -137,10 +150,15 @@ const spots = [
       <p>The Digitech Centre, opened in 2021, is a partnership between BT and the University of Suffolk. It provides undergraduate and postgraduate teaching across a wide range of digital technology disciplines.</p>
 
 <br><p><strong>Listen to Tim Whitley discuss the new centre:</strong><br>
-<audio controls src="audio/digitech2.mp3"></audio></p>
+<br><audio controls src="audio/digitech2.mp3"></audio></p>
 
 <br><p><strong>Listen to Meral Bence describe how the centre was created:</strong><br>
-<audio controls src="audio/digitech.mp3"></audio></p>
+<br><audio controls src="audio/digitech.mp3"></audio></p>
+<br><br><img src="images/7-digitech1.jpg" class="info-img">
+<br><br><img src="images/7-digitech2.jpg" class="info-img">
+<br><br><img src="images/7-digitech3.jpg" class="info-img">
+
+
 `,
   },
   {
@@ -152,7 +170,7 @@ const spots = [
     <p>Through the trees ahead are the satellite dishes of Martlesham Teleport. Once operated by BT and now by Arqiva, the Teleport is used by broadcasters worldwide to route content between locations. The funeral of Diana, Princess of Wales, was relayed globally from here in 1997. It remains the UK’s most easterly satellite earth station.</p>
 
 <br><p><strong>Listen to Jon Wakeling discuss the teleport’s history:</strong><br>
-<audio controls src="audio/teleport.mp3"></audio></p>
+<br><audio controls src="audio/teleport.mp3"></audio></p>
           `,
   },
   {
@@ -164,7 +182,8 @@ const spots = [
      <p>This area was once used to store emergency exchange hardware for rural areas — equipment that could be deployed following fire, flood, or major outages. Technology has since moved on, and the area now supports research into space communications.</p>
 
 <br><p><strong>Listen to Jon Wakeling talk about the area:</strong><br>
-<audio controls src="audio/romes.mp3"></audio></p>
+<br><audio controls src="audio/romes.mp3"></audio></p>
+<br><br><img src="images/9-romes.jpg" class="info-img">
           `,
   },
   {
@@ -176,7 +195,8 @@ const spots = [
        <p>This experimental Smart Home is being developed by BT and the University of Suffolk as part of the Digitech Project. It enables research and testing into future building technologies, sustainable living, and digital construction.</p>
 
 <br><p><strong>Listen to Meral Bence describe this new facility:</strong><br>
-<audio controls src="audio/smarthome.mp3"></audio></p>
+<br><audio controls src="audio/smarthome.mp3"></audio></p>
+
        `,   
   },
   {
@@ -188,8 +208,11 @@ const spots = [
      <p>The Sports Hall supports health and wellbeing through fitness and team activities. It contains facilities for basketball, netball, badminton, fitness classes, judo, and even indoor model aeroplane flying. Famous sporting visitors have included Austin Healey, Lawrence Dallaglio, Sam Quek, Daley Thompson, and the GB Archery Team.</p>
 
 <br><p><strong>Listen to Bernie Fenn talk about sport at Adastral Park:</strong><br>
-<audio controls src="audio/sports.mp3"></audio></p>
-          `,
+<br><audio controls src="audio/sports.mp3"></audio></p>
+<br><br><img src="images/11-sports1.jpg" class="info-img">          
+<br><br><img src="images/11-sports2.jpg" class="info-img">
+<br><br><img src="images/11-sports3.jpg" class="info-img">
+`,
   },
   {
     name: "Gaming",
@@ -200,7 +223,7 @@ const spots = [
        <p>Wireplay was an online multiplayer gaming network developed by BT in the 1990s. It allowed PC gamers to play against each other remotely using a dial-up internet connection — a pioneering service at the time.</p>
 
 <br><p><strong>Listen to Chris Adams talk about the development of Wireplay:</strong><br>
-<audio controls src="audio/wireplay.mp3"></audio></p>
+<br><audio controls src="audio/wireplay.mp3"></audio></p>
           `,
   },
   {
@@ -212,16 +235,16 @@ const spots = [
      <p>Adastral Park has its own emergency services team, including fire and ambulance capabilities.</p>
 
 <br><p><strong>Listen to Mike Warden talk about the formation of the BT Emergency Team in 1977:</strong><br>
-<audio controls src="audio/formation.mp3"></audio></p>
+<br><audio controls src="audio/formation.mp3"></audio></p>
 
 <br><p><strong>Listen to Mike describe the team’s first fire engine:</strong><br>
-<audio controls src="audio/engine.mp3"></audio></p>
+<br><audio controls src="audio/engine.mp3"></audio></p>
 
 <br><p><strong>Mike recalls emergency incidents at the Park:</strong><br>
-<audio controls src="audio/emergencies.mp3"></audio></p>
+<br><audio controls src="audio/emergencies.mp3"></audio></p>
 
 <br><p><strong>Bernie Fenn talks about a worrying power incident:</strong><br>
-<audio controls src="audio/power.mp3"></audio></p>
+<br><audio controls src="audio/power.mp3"></audio></p>
           `,
 
   },
@@ -234,10 +257,10 @@ const spots = [
        <p>The Network Operations Centre (NoC) manages BT’s national network — including broadband, leased lines, Ethernet circuits, and mobile backhaul for millions of customers.</p>
 
 <br><p><strong>Listen to Andy Skingley discuss the history of the NoC:</strong><br>
-<audio controls src="audio/nocintro.mp3"></audio></p>
+<br><audio controls src="audio/nocintro.mp3"></audio></p>
 
 <br><p><strong>Listen to Andy outline situations where the NoC resolves network issues:</strong><br>
-<audio controls src="audio/nocscenarios.mp3"></audio></p>
+<br><audio controls src="audio/nocscenarios.mp3"></audio></p>
           `,
   },
   {
@@ -248,8 +271,6 @@ const spots = [
      info: `
      <p>Innovation Martlesham is a thriving ecosystem of ICT and digital companies, launched in 2011. It was created to support collaboration, creativity, and the development of pioneering digital technologies.</p>
 
-<br><p><strong>Listen to Nicky Daniels discuss Innovation Martlesham:</strong><br>
-<audio controls src="audio/placeholder24.mp3"></audio></p>
           `,
   },
   {
@@ -261,8 +282,10 @@ const spots = [
        <p>This is the last spot on the trail. Congratulations — you have reached the final spot on the trail! The new Adastral Park Heritage Centre showcases the history of the site and the groundbreaking work carried out here over the decades. Take your time exploring this unique facility.</p>
 
 <br><p><strong>Listen to Terry Henshall talk about the new centre:</strong><br>
-<audio controls src="audio/heritage.mp3"></audio></p>
-          `,
+<br><audio controls src="audio/heritage.mp3"></audio></p>
+    <br><br><img src="images/heritage1.jpg" class="info-img">
+    <br><br><img src="images/heritage2.jpg" class="info-img">
+    `,
   }
 ];
 
